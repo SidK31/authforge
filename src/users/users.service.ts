bfbd86ac403fa4108e41b9a250918c69a5d1e124
Mohammad Khwahish Siddiqui <mdkhwahishsid@gmail.com>
2026-09-07 15:41:38 +0530

    style: format users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
 
 @Injectable()
